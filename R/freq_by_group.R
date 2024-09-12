5428e2b078710b176d3abf9107de51cc83ea98ff
Simin Liu <108092745+sfrer@users.noreply.github.com>
2024-09-11 22:08:47 -0400

    Modify freq_by_group

--- R/freq_by_group.R
+++ R/freq_by_group.R
@@ -6,7 +6,7 @@
 #'@param column The name of the column to look at.
 #'@param decreasing A logical value to determine the order of the frequency table. Default to TRUE.
 #'@param graph A logical value to determine whether to plot the frequency table. Default to TRUE.
-#'@param group The name of the column to group by. Not a character input. Use the column name directly.
+#'@param group The name of the column to group by.
 #'@param fill The color to plot. Default to pink.
 #'@param num The number of top items to plot. Default to 10.
 #'@import ggplot2
@@ -17,18 +17,21 @@
 #'library(patents)
 #'data('patent_data')
 #'x1=clean_by(patent_data, "inventorState")
-#'x1=freq_by_group(x1, inventorState, "inventorsName", graph=FALSE)
+#'x1=freq_by_group(x1, "inventorState", "inventorsName", graph=FALSE)
 freq_by_group=function(dataset, group, column, decreasing=TRUE, graph=TRUE, num=10, fill='pink'){
   # goal: take in a dataset and return the frequency of the specified column for each group
   # input: dataset: the dataset to look at
-  #        group: the column to group by
+  #        group(chr): the column to group by
   #        column(chr): the column to look at
   #        graph: whether to plot the frequency table
   #        num: the number of top items to plot
   # designed for inventorState, assigneeName, and inventorName
   # output: a dataframe with the frequency of the specified column for each group
   require(dplyr)
+  require(ggplot2)
+  require(rlang)
   # split groups
+  group=sym(group)
   group=enquo(group)
   dataset_list=dataset %>% group_by(!!group) %>% group_split()
 
